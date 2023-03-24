162af2579c021460e314fa76e8e766e424988bda
indresh0564 <indresh0564@gmail.com>
2023-03-25 00:20:05 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,42 @@
+/*console.log(Document.domain);
+console.log(document.URL);
+console.log(document.title);
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+console.log(document.all);
+console.log(document.all[10].textContent);
+console.log(document.forms[0]);
+console.log(document.links);
+console.log(document.images);*/
+
+//getelementbyid//
+
+// console.log(document.getElementById('header-title'));
+// var headertitle = document.getElementById('header-title');
+// console.log(headertitle);
+// headertitle.textContent = 'hello';
+// headertitle.innerText = 'Goodbye';
+// console.log(headertitle.innerText);
+// headertitle.innerHTML = '<h1>hello</h1>'; 
+// var header = document.getElementById('main-header');
+// header.style.borderBottom = 'solid 3px #000';
+
+//getElementByClassName//
+var item = document.getElementsByClassName('list-group-item');
+console.log(item);
+// item[1].textContent = 'Hello 2';
+item[1].style.fontWeight = 'bold';
+item[2].style.backgroundColor = 'green';
+// item.style.backgroundColor = '#f4f4f4';
+for(var i=0; i<item.length; i++)
+{
+    item[i].style.fontWeight = 'bold';  
+}
+
+
+
+
+
+
+
